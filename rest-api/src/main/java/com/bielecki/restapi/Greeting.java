7c7abf60a18cc755e4f046efab2409c287f3c4dc
kerukeru <keru@protonmail.com>
2021-03-11 17:44:39 +0100

    Initial commit - hello world in REST & Maven

---
+++ rest-api/src/main/java/com/bielecki/restapi/Greeting.java
@@ -0,0 +1,15 @@
+package com.bielecki.restapi;
+
+public class Greeting {
+
+    private String msg;
+
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
---
+++ rest-api/src/main/java/com/bielecki/restapi/HelloWorlRestController.java
@@ -0,0 +1,15 @@
+package com.bielecki.restapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorlRestController {
+
+    @GetMapping("/api/hello")
+    public Greeting hello(){
+        Greeting greeting = new Greeting();
+        greeting.setMsg("hello");
+        return greeting;
+    }
+}
