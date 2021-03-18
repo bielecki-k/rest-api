5820194ebe2492eba733cef5b18e6c058fbd263e
kerukeru <keru@protonmail.com>
2021-03-18 21:08:57 +0100

    query params

--- rest-api/src/main/java/com/bielecki/restapi/maturity/methods/DocumentService.java
+++ rest-api/src/main/java/com/bielecki/restapi/maturity/methods/DocumentService.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController("documentServiceL2")
 @RequestMapping("/api/maturity/l2/documents")
@@ -14,9 +15,19 @@ public class DocumentService {
 
     private List<Document> documents = DataFixtureUtils.initDocuments();
 
-    @GetMapping
-    public ResponseEntity<List<Document>> getAllDocuments(){
-        return ResponseEntity.ok().header("Cache-Control", "max-age"+"=3600").body(documents);
+    @GetMapping(params = {"title","number"})
+    public ResponseEntity<List<Document>> getDocumentsByTitleAndNumber(@RequestParam("title") String title,
+                                                                       @RequestParam("number") long number){
+
+        return ResponseEntity.ok()
+                .header("Cache-Control", "max-age"+"=3600")
+                .body(documents.stream()
+                        .filter(doc -> title.equals(doc.getTitle()) && number==doc.getNumber())
+                        .collect(Collectors.toList())
+                )
+                ;
+        //postman:
+        //http://localhost:8080/api/maturity/l2/documents?title=doc1&number=0
     }
 
     @GetMapping(produces = MediaType.TEXT_PLAIN_VALUE )
